9cbb125ea0b87c53e69bb8c775186d4e9d9f00e5
MariannaSarkisian <spn.mjj@gmail.com>
2022-12-10 17:24:21 +0300

    starting python

---
+++ lec.py
@@ -0,0 +1,15 @@
+#print ('hello') # start
+value=None
+a=123
+b=1.23
+#print (type(a))
+#print(b)
+value=1234
+#print(value)
+
+s='hello \nworld'
+#print (s)
+
+print(a,'-',b,'-',s)
+print('{1}-{2}-{0}'.format(a,b,s))
+print(f'{a}-{b}-{s}')
