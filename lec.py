0d73aec12902f7a79a72cebb15e3965c5868dd4b
MariannaSarkisian <spn.mjj@gmail.com>
2022-12-10 18:21:26 +0300

    before if

--- lec.py
+++ lec.py
@@ -1,15 +1,72 @@
 #print ('hello') # start
-value=None
-a=123
-b=1.23
+#value=None
+#a=123
+#b=1.23
 #print (type(a))
 #print(b)
-value=1234
+#value=1234
 #print(value)
 
-s='hello \nworld'
+#s='hello \nworld'
 #print (s)
 
-print(a,'-',b,'-',s)
-print('{1}-{2}-{0}'.format(a,b,s))
-print(f'{a}-{b}-{s}')
+#print(a,'-',b,'-',s)
+#print('{1}-{2}-{0}'.format(a,b,s))
+#print(f'{a}-{b}-{s}')
+
+
+#f=True
+#print(f)
+
+#list=[1,2,3]
+#print(list)
+#col =['1','3','hello']
+#print(col)
+
+#ввод и вывод данных
+
+#print ('enter a')
+#a=int(input())
+#print('enter b')
+#b=int(input())
+#print(a,' ',b,' ',a+b )
+#print('{} {}'.format(a,b))
+#print(f'{a} {b}')
+
+#Арифметические операции
+#+,-,/,%,*,//,** stepen
+#a=1.34438
+#b=3
+#c= round(a*b,5)
+#print(c)
+
+#a=3
+#a+=5
+#print(a)
+
+#логические операции
+#>,<,>=,==<,!= .  is,is not ,and, or
+#is,is not,in ,not in
+#gen
+
+#a=1!=5
+#print(a)
+
+#aa=1<3<5
+#print(aa)
+
+#func=1
+#T=4
+#X=32
+#print(func>T>(x))
+
+#f=[1,2,3,4]
+#print(f)
+#print (2 in f)
+
+
+
+#If 
+#if Else
+
+
